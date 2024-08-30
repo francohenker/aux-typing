ae8e5b288823c10bc7013bfbaa1873ff8a8265c0
ju1010 <alvaroleonelaguirre@hotmail.com>
2024-08-30 20:12:38 -0300

    added a leaderboard

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,26 +1,3 @@
-<<<<<<< HEAD
-import React from "react";
-import "./App.css";
-
-const App = () => {
-  return (
-    <div className="container">
-      <div className="auth-container">
-        <h2>Login / Register</h2>
-        <button className="auth-button">Login</button>
-        <button className="auth-button">Register</button>
-      </div>
-      <div className="challenges-container">
-        <h2>Challenges</h2>
-        <div className="challenges-grid">
-            {Array.from({ length: 10}).map((_, index) => (
-              <div key={index} className="challenge-box">
-                Challenge {index + 1}
-              </div>
-            ))}
-        </div>
-      </div>
-=======
 import React, { useState } from 'react';
 import './index.css'; 
 import LeaderBoard from './LeaderBoard';
@@ -74,7 +51,6 @@ function App() {
       <div className="w-full max-w-4xl mt-6 font-semibold">
         <LeaderBoard users={users} />
       </div>
->>>>>>> chises
     </div>
   )
 };
