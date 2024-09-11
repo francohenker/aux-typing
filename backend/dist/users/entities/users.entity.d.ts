77f332cea9a7ec9c8503e0c671f4e62a7cd352ba
Franco Henker <francohenker@gmail.com>
2024-09-11 20:16:22 -0300

    update dependecies

--- backend/dist/users/entities/users.entity.d.ts
+++
@@ -1,9 +0,0 @@
-export declare class Users {
-    id: number;
-    nickname: string;
-    password: string;
-    admin: boolean;
-    validatePassword(password: string): Promise<boolean>;
-    hashPassword(): Promise<void>;
-    constructor(nickname: string, password: string, admin: boolean);
-}
