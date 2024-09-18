842a9659a95d6b01ba9f7c17566c27013ac095a0
Franco Henker <francohenker@gmail.com>
2024-09-18 19:22:41 -0300

    endpoint ptu fixed

--- backend/src/phrase-to-user/dto/phrase-to-user-create.dto.ts
+++ backend/src/phrase-to-user/dto/phrase-to-user-create.dto.ts
@@ -1,4 +1,6 @@
 import { IsNotEmpty } from "class-validator";
+import { Phrase } from "src/phrase/entities/phrase.entity";
+import { Users } from "src/users/entities/users.entity";
 
 export class PhraseToUserCreateDto {    
     @IsNotEmpty()
@@ -16,6 +18,28 @@ export class PhraseToUserCreateDto {
     constructor(phraseId: number, userId: number, wpm: number, completed: boolean) {
         this.phraseId = phraseId;
         this.userId = userId;
+        this.wpm = wpm;
+        this.completed = completed;
+    }
+}
+
+
+export class PhraseToUserDto {
+    @IsNotEmpty()
+    userId: Users;
+
+    @IsNotEmpty()
+    phraseId: Phrase;
+
+    @IsNotEmpty()
+    wpm: number;    
+
+    @IsNotEmpty()
+    completed: boolean
+    
+    constructor(userId: Users, phraseId: Phrase, wpm: number, completed: boolean) {
+        this.userId = userId;
+        this.phraseId = phraseId;
         this.wpm = wpm;
         this.completed = completed;
     }
--- backend/src/phrase-to-user/phrase-to-user.service.ts
+++ backend/src/phrase-to-user/phrase-to-user.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { PhraseToUsers } from './entities/phrase-to-users.entity';
 import { In, Repository } from 'typeorm';
-import { PhraseToUserCreateDto } from './dto/phrase-to-user-create.dto';
+import { PhraseToUserCreateDto, PhraseToUserDto } from './dto/phrase-to-user-create.dto';
 import { UsersService } from 'src/users/users.service';
 import { PhraseService } from 'src/phrase/phrase.service';
 import { Phrase } from 'src/phrase/entities/phrase.entity';
@@ -23,19 +23,32 @@ export class PhraseToUserService {
 
 
     async createPhraseToUser(phraseToUser: PhraseToUserCreateDto): Promise<void> {
-        const user = await this.usersRepository.findOneBy({id: phraseToUser.userId});
+        const user = await this.usersRepository.findOneBy({
+            id: phraseToUser.userId
+        });
         
         if(!user){
             throw new Error('User not found');
         }
         
-        const phrase = await this.phraseRepository.findOneBy({id: phraseToUser.phraseId});
+        const phrase = await this.phraseRepository.findOneBy({
+            id: phraseToUser.phraseId
+        });
+
         if(!phrase){
             throw new Error('Phrase not found');
         }
 
-        this.phraseToUsersRepository.create(phraseToUser);
-        await this.phraseToUsersRepository.save(phraseToUser);
+        // const a = new PhraseToUserDto(user, phrase, phraseToUser.wpm, phraseToUser.completed);         
+        
+        // const ptu = this.phraseToUsersRepository.create(phraseToUser);
+        const ptu = this.phraseToUsersRepository.create({
+            user: user,  // Asignar la relación de usuario
+            phrase: phrase,  // Asignar la relación de phrase
+            wpm: phraseToUser.wpm,
+            completed: phraseToUser.completed
+        });
+        await this.phraseToUsersRepository.save(ptu);
         
     }
 }
