78abf427648ff6dfa9236d7d3da593178cfa18bc
Franco Henker <francohenker@gmail.com>
2024-09-19 15:23:21 -0300

    add counterWpm component

---
+++ front-end/src/pages/CounterWpm.jsx
@@ -0,0 +1,48 @@
+import React, { useState, useEffect } from 'react';
+
+function CounterWpm({inputText, originalWords = []}) {
+  const [wpm, setWpm] = useState(0);
+  const [wordsTyped, setWordsTyped] = useState(0);
+  const [timePassed, setTimePassed] = useState(0);
+  const [accuracy, setAccuracy] = useState(100);
+
+  // Calcular WPM cada segundo basado en el número de palabras y el tiempo transcurrido
+  useEffect(() => {
+    const interval = setInterval(() => {
+
+      setTimePassed((prevTime) => prevTime + 1);
+
+      const wordsTyped = inputText.trim() ? inputText.trim().split(/\s+/) : [];
+      const completedWords = wordsTyped.slice(0, originalWords.length);
+      const correctWords = completedWords.filter((word, index) => word === originalWords[index]).length;
+      // const correctWords = wordsTyped.filter((word, index) => word === originalWords[index]).length;
+      // const accuracyRate = wordsTyped.length > 0 ? (correctWords / wordsTyped.length) * 100 : 100;
+      const accuracyRate = completedWords.length > 0 ? (correctWords / completedWords.length) * 100 : 100;
+
+      setAccuracy(accuracyRate);
+      const wordsArray = inputText.trim().split(/\s+/); // Dividir palabras por espacios
+
+      const wordCount = wordsArray.filter(Boolean).length; // Contar palabras no vacías
+      setWordsTyped(wordCount);
+
+      if (timePassed > 0) {
+        setWpm(Math.round((wordCount / timePassed) * 60)); // WPM = palabras / minutos
+      }
+    }, 200); //tasa de actualizacion
+
+    return () => clearInterval(interval);
+  }, [inputText, originalWords]);
+
+  return (
+    <div className="">
+      <div className="bg-white p-4 rounded-lg shadow-md max-w-xs">
+        <h2 className="text-2xl font-bold mb-4 text-orange-600">Your words per minute</h2>
+        <p className="text-3xl font-bold mb-2 text-orange-600">{wpm}</p>
+        <h2 className="text-xl font-bold mb-2 text-orange-600">Accuracy</h2>
+        <p className="text-2xl font-bold mb-2 text-green-600">{accuracy}%</p>
+      </div>
+    </div>
+  );
+}
+
+export default CounterWpm;
--- front-end/src/pages/TypingTestPage.js
+++ front-end/src/pages/TypingTestPage.js
@@ -1,11 +1,10 @@
 import React, { useState, useEffect, useRef } from 'react';
 import '../styles/TypingTestPage.css';
+import CounterWpm from './CounterWpm';
 
 function TypingTestPage() {
   const [words, setWords] = useState([]);
   const [typedWords, setTypedWords] = useState('');
-  const [startTime, setStartTime] = useState(null);
-  const [wpm, setWpm] = useState(0);
   const textareaRef = useRef(null);
 
   useEffect(() => {
@@ -30,18 +29,7 @@ function TypingTestPage() {
   };
 
   const handleTyping = (e) => {
-    if (!startTime) {
-      setStartTime(new Date());
-    }
     setTypedWords(e.target.value);
-
-    // Calcula el WPM cuando el usuario termina de escribir todas las palabras
-    if (typedWords.trim().split(' ').length === words.length) {
-      const endTime = new Date();
-      const timeDiff = (endTime - startTime) / 1000 / 60;
-      const wordsPerMinute = Math.floor(words.length / timeDiff);
-      setWpm(wordsPerMinute);
-    }
   };
 
   const handleKeyPress = (e) => {
@@ -97,20 +85,25 @@ function TypingTestPage() {
 
   return (
     <div className="typing-test-container">
-      <h2 className="text-2xl font-bold mb-4 text-orange-600">Test de Tecleo</h2>
+      <h2 className="text-3xl font-bold mb-4 text-orange-600">Test de Tecleo</h2>
+
+      <div className="counter-container">
+        <CounterWpm inputText={typedWords} />
+      
+      </div>
+      
       <div className="words-container">
         {renderWords()}
       </div>
+
       <textarea
         ref={textareaRef}
         className="typing-input"
         placeholder="Escribe aquí..."
         value={typedWords}
-        onChange={handleTyping}
+        onChange={handleTyping} // Actualiza el estado typedWords
       />
-      <div className="wpm-display">
-        {wpm > 0 && <p>Palabras por minuto: {wpm}</p>}
-      </div>
+     
       <div className="keyboard-container">
         <div className="keyboard-row">
           {'qwertyuiop'.split('').map((letter) => (
@@ -119,6 +112,7 @@ function TypingTestPage() {
             </div>
           ))}
         </div>
+      
         <div className="keyboard-row">
           {'asdfghjklñ'.split('').map((letter) => (
             <div key={letter} id={letter} className="key">
@@ -127,6 +121,7 @@ function TypingTestPage() {
           ))}
         </div>
         <div className="keyboard-row">
+              
           {'zxcvbnm'.split('').map((letter) => (
             <div key={letter} id={letter} className="key">
               {letter}
